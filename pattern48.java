16e27b1b19c0cb4ab36e7427fd0275e8e23bf27f
Vipul Kale <120079040+Vipulkale45@users.noreply.github.com>
2025-08-12 13:55:16 +0530

    Add files via upload

---
+++ pattern48.java
@@ -0,0 +1,28 @@
+/*
+    *
+   **
+  ***
+ ****
+*****
+*/
+class p48
+{
+ public static void main(String[] args)
+  {
+     int n=5;
+     for(int i=1; i<=n; i++ )
+	{
+            for(int j=i; j<n; j++)
+		{
+		  System.out.print(" ");
+		}
+	    for(int j=1; j<=i; j++)
+		{
+			System.out.print(i);
+	
+		}
+		System.out.println();
+
+	}
+  }
+}
